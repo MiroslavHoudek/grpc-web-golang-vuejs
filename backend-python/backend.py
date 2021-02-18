8b53810da12ffb75b4916bdc657af4e0dc718d3c
Miroslav Houdek <miroslav.houdek@locksley.cz>
2021-02-18 19:14:58 +0100

    Added envoy, it partially works now

--- backend-python/backend.py
+++ backend-python/backend.py
@@ -2,41 +2,47 @@ from concurrent import futures
 import grpc
 import logging
 
-import daphne.server
-import sonora.asgi
-from asgi_cors import asgi_cors
 import asyncio
 
 import accounts_pb2
 import accounts_pb2_grpc
 
-class AccountServicer(accounts_pb2_grpc.AccountServiceServicer):
-    """Provides methods that implement functionality of route guide server."""
+class AccountServiceServicer(accounts_pb2_grpc.AccountServiceServicer):
+    """Provides methods that implement functionality of account server."""
 
     def __init__(self):
         #self.db = route_guide_resources.read_route_guide_database()
         pass
 
-    def GetFeature(self, request, context):
-        feature = get_feature(self.db, request)
-        if feature is None:
-            return route_guide_pb2.Feature(name="", location=request)
-        else:
-            return feature
+    def Create(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        print("Create called")
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def AuthenticateByEmailAndPassword(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        print("Authenticate called")
+        #context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        #context.set_details('Method not implemented!')
+        return accounts_pb2.Account(token="cau")
+
+    def ChangePassword(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        print("ChangePass called")
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
 def serve():
-
-    grpc_asgi_app = sonora.asgi.grpcASGI()
-    print("app1 {}", grpc_asgi_app)
-
-    server = daphne.server.Server(grpc_asgi_app, ["tcp:port=9000:interface=0.0.0.0"], verbosity=10 )
-
-    grpc_asgi_app = asgi_cors(grpc_asgi_app, allow_all=True)
-    print("app2 {}", grpc_asgi_app)
-
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     accounts_pb2_grpc.add_AccountServiceServicer_to_server(
-        accounts_pb2_grpc.AccountServiceServicer(), grpc_asgi_app.__wrapped__)
-    server.run()
+        AccountServiceServicer(), server)
+    server.add_insecure_port('[::]:8081')
+    server.start()
+    print("Server started")
+    server.wait_for_termination()
 
 if __name__ == '__main__':
     logging.basicConfig()
